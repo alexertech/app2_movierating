672804ba72757140c6296def0718e35074dcd774
Alex B <>
2017-10-27 00:28:59 -0300

    Users Sessions and Login process

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,47 +1,49 @@
 class MoviesController < ApplicationController
 
-        before_action :find_movie, only: [:show, :edit, :update, :destroy]
-        
-        def index
-            @movies = Movie.all
-        end
+    before_action :authorize, only: [:new, :create,:edit, :update, :destroy]
+    before_action :find_movie, only: [:show, :edit, :update, :destroy]
     
-        def new
-            @movie = Movie.new
-        end
-        
-        def create
-            @movie = Movie.new(params_movie)
     
-            @movie.save
-            
-            redirect_to movies_path
-        end
-    
-        def show
-        end
-    
-        def edit
-        end
-    
-        def update
-            @movie.update(params_movie)
-            redirect_to movies_path
-        end
-    
-        def destroy
-            @movie.destroy
-            redirect_to movies_path
-        end
-    
-        private
-    
-        def params_movie
-            params.require(:movie).permit(:title,:summary)
-        end
-    
-        def find_movie
-            @movie = Movie.find(params[:id])
-        end
+    def index
+        @movies = Movie.all
     end
+
+    def new
+        @movie = Movie.new
+    end
+    
+    def create
+        @movie = Movie.new(params_movie)
+
+        @movie.save
+        
+        redirect_to movies_path
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+        @movie.update(params_movie)
+        redirect_to movies_path
+    end
+
+    def destroy
+        @movie.destroy
+        redirect_to movies_path
+    end
+
+    private
+
+    def params_movie
+        params.require(:movie).permit(:title,:summary)
+    end
+
+    def find_movie
+        @movie = Movie.find(params[:id])
+    end
+end
     
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+    def self.authenticate(email, password)
+
+        user = User.where(email: email).first
+
+        if user && user.password == password
+            user
+        else
+            nil
+        end
+    end
+
 end
